97217a12ab1dade9ca20ef92a625e2785c124a84
Ismoil Muhammadiev <ism0il.muhammadiev92@gmail.com>
2023-02-21 14:41:07 +0500

    Домашняя работа номер 3. Выполнена Все задание.

---
+++ src/Lesson3/Drink.java
@@ -0,0 +1,28 @@
+package Lesson3;
+
+public class Drink {
+    protected String name;
+    protected double cost;
+
+    public Drink(){
+
+    }
+
+    public Drink(String name, double cost){
+        this.name=name;
+        this.cost = cost;
+    }
+
+    protected String getName(){
+        return this.name;
+    }
+
+    protected double getCost(){
+        return this.cost;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Name:%s, Cost:%f", this.name, this.cost);
+    }
+}
