d3047aade41b6a945ae668ce74045ea36e3991e5
Ismoil Muhammadiev <ism0il.muhammadiev92@gmail.com>
2023-02-22 14:08:08 +0500

    Домашняя работа номер 4. Выполнена Все задание.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import Lesson4.ComputerIterator;
+import Lesson4.NoteBook;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        NoteBook notebook1 = new NoteBook(1, 4, 256, "Windows 10", "White");
+        NoteBook notebook2 = new NoteBook(2, 16, 1024, "Windows 10", "Black");
+        NoteBook notebook3 = new NoteBook(3, 32, 2048, "Linux", "Gray");
+        NoteBook notebook4 = new NoteBook(4, 8, 512, "Mac OS", "Black");
+
+        ComputerIterator<NoteBook> notebookIterator = new ComputerIterator<>();
+
+        notebookIterator.AddComputer(notebook1);
+        notebookIterator.AddComputer(notebook2);
+        notebookIterator.AddComputer(notebook3);
+        notebookIterator.AddComputer(notebook4);
+
+        while (notebookIterator.hasNext()){
+            System.out.println(notebookIterator.next());
+        }
     }
 }
