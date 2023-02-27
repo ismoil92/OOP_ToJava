a57c93bc0bf049c1fd0149b51fc5e0813035c499
Ismoil Muhammadiev <ism0il.muhammadiev92@gmail.com>
2023-02-27 17:53:41 +0500

    Домашняя работа номер 5. Выполнена паттерн Builder.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import Lesson5.Building;
+import Lesson5.CreateBuildingBuilder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Building building = CreateBuildingBuilder.getInstance().setHeight(12.3).setNumberOfStoreys(2).setNumberOfFlats(3).setEntrances(5).build();
+
+        System.out.println(building);
     }
 }
